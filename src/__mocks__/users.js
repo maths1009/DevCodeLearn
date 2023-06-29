69f53f895ecd77270d8a7a31e392156d4295ec50
Mathis <lb.brangeon@gmail.com>
2023-06-29 10:48:03 +0200

    test ci

---
+++ src/__mocks__/users.js
@@ -0,0 +1,40 @@
+export const notificatons = [
+  {
+    name: "Administateur",
+    message:
+      "Bonjour, je suis l'administrateur du site, si vous avez des questions n'hésitez pas à me contacter",
+    date: "12/12/2020",
+    online: true,
+    avatar: "avatar1.png",
+  },
+  {
+    name: "Administateur",
+    message:
+      "Votre cours a été mis à jour, vous pouvez vérifier le nouveau cours dans votre parcours d'études.",
+    date: "12/12/2020",
+    online: true,
+    avatar: "avatar1.png",
+  },
+  {
+    name: "Administateur",
+    message: "Félicitations, vous avez terminé le cours de 'Développement Web'",
+    date: "12/12/2020",
+    online: true,
+    avatar: "avatar1.png",
+  },
+];
+
+export const messages = [
+  {
+    name: "Mathis.b",
+    date: "12/12/2020",
+  },
+  {
+    name: "Administateur",
+    date: "12/12/2020",
+  },
+  {
+    name: "Hugo.R",
+    date: "12/12/2020",
+  },
+];
